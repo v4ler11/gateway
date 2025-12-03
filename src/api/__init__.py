0fd23ba29804e8ce187afbba21607098f7d9680c
V4LER11 <work@valerii.cc>
2025-12-03 13:28:25 +0000

    remove api

--- src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from api.client import Client
-from api.aclient import AClient
-from api.utils import collect_chunks_streaming_audio, ChunksAudio
-
-from core.routers.oai.schemas import ChatPost, ChatPostAudio
-from core.routers.oai.models import ChatMessageUser, ChatMessageSystem
