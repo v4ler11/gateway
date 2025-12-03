19ab132e3d353a5dce431400a7fb5c475e5f2175
V4LER11 <work@valerii.cc>
2025-12-03 12:45:12 +0000

    less check in globals

--- src/core/globals.py
+++ src/core/globals.py
@@ -9,11 +9,5 @@ TOKENIZERS_DIR = MODEL_CACHE_DIR / "tokenizers"
 MODELS_DIR = MODEL_CACHE_DIR / "models"
 
 YAML_CONFIG = BASE_DIR / "config.yaml"
-if not YAML_CONFIG.is_file():
-    raise Exception(
-        f"config.yaml doesn't exist, create one using config.example.yaml as a reference; "
-        f"seeing files: {', '.join([str(d) for d in BASE_DIR.iterdir()])}"
-    )
-
 
 PORT = 8000
--- src/core/main.py
+++ src/core/main.py
@@ -3,14 +3,25 @@ import asyncio
 import uvloop
 import uvicorn
 
+from core import BASE_DIR
 from core.app import App
-from core.globals import LOGS_DIR, PORT
+from core.globals import LOGS_DIR, PORT, YAML_CONFIG
 from core.logger import init_logger, info
 from core.status.worker import spawn_worker as spawn_status_worker
 from models.config import Config, models_from_config
 
 
+def checks():
+    if not YAML_CONFIG.is_file():
+        raise Exception(
+            f"config.yaml doesn't exist, create one using config.example.yaml as a reference; "
+            f"seeing files: {', '.join([str(d) for d in BASE_DIR.iterdir()])}"
+        )
+
+
 def main():
+    checks()
+
     init_logger(LOGS_DIR)
     info("Logger initialized")
 
